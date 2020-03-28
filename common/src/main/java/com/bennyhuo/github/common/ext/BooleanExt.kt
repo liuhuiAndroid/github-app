b19db537313b7637f28730a98c9d37188efa2300
LiuHuiAndroid <642404044@qq.com>
2020-03-29 00:56:19 +0800

    kotlin 2

--- common/src/main/java/com/bennyhuo/github/common/ext/BooleanExt.kt
+++ common/src/main/java/com/bennyhuo/github/common/ext/BooleanExt.kt
@@ -1,29 +1,33 @@
 package com.bennyhuo.github.common.ext
 
-sealed class BooleanExt<out T>
-
-object Otherwise: BooleanExt<Nothing>()
-class WithData<T>(val data: T): BooleanExt<T>()
-
-inline fun <T> Boolean.yes(block: ()->T) =
-    when {
-        this -> {
-            WithData(block())
-        }
-        else -> {
-            Otherwise
-        }
-    }
-
-inline fun <T> Boolean.no(block: () -> T) = when {
-    this -> Otherwise
-    else -> {
-        WithData(block())
-    }
+/**
+ * sealed	英[siːld]	美[siːld]
+ * 密封类：为父类添加一个sealed修饰符，对可能创建的子类做出严格的限制。
+ * sealed修饰符隐含的这个类是一个open类
+ * out：表示类型只能输出不能输入
+ */
+sealed class BooleanExt<out T> {
+    // object 定义一个类并同时创建一个实例，单例
+    object Otherwise : BooleanExt<Nothing>()
+    class WithData<T>(val data: T) : BooleanExt<T>()
 }
 
-inline fun <T> BooleanExt<T>.otherwise(block: ()->T): T =
-        when(this){
-            is Otherwise -> block()
-            is WithData -> this.data
+// inline 内联函数：减少资源开销
+inline fun <T> Boolean.yes(block: () -> T) =
+        when {
+            this -> BooleanExt.WithData(block())
+            else -> BooleanExt.Otherwise
+        }
+
+inline fun <T> Boolean.no(block: () -> T) =
+        when {
+            this -> BooleanExt.Otherwise
+            else -> BooleanExt.WithData(block())
+        }
+
+inline fun <T> BooleanExt<T>.otherwise(block: () -> T): T =
+        // when表示式中处理所有sealed类的子类，不需要提供默认分支
+        when (this) {
+            is BooleanExt.Otherwise -> block()
+            is BooleanExt.WithData -> this.data
         }
--- common/src/main/java/com/bennyhuo/github/common/sharedpreferences/PreferencesExt.kt
+++ common/src/main/java/com/bennyhuo/github/common/sharedpreferences/PreferencesExt.kt
@@ -4,8 +4,14 @@ import android.content.Context
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
-class Preference<T>(val context: Context, val name: String, val default: T, val prefName: String = "default")
-    : ReadWriteProperty<Any?, T>{
+/**
+ * ReadWriteProperty：读写代理
+ */
+class Preference<T>(val context: Context,
+                    val name: String,
+                    val default: T,
+                    private val prefName: String = "default")
+    : ReadWriteProperty<Any?, T> {
 
     private val prefs by lazy {
         context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
@@ -15,10 +21,15 @@ class Preference<T>(val context: Context, val name: String, val default: T, val 
         return findPreference(findProperName(property))
     }
 
-    private fun findProperName(property: KProperty<*>) = if(name.isEmpty()) property.name else name
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        putPreference(findProperName(property), value)
+    }
 
-    private fun findPreference(key: String): T{
-        return when(default){
+    private fun findProperName(property: KProperty<*>) =
+            if (name.isEmpty()) property.name else name
+
+    private fun findPreference(key: String): T {
+        return when (default) {
             is Long -> prefs.getLong(key, default)
             is Int -> prefs.getInt(key, default)
             is Boolean -> prefs.getBoolean(key, default)
@@ -27,13 +38,10 @@ class Preference<T>(val context: Context, val name: String, val default: T, val 
         } as T
     }
 
-    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
-         putPreference(findProperName(property), value)
-    }
-
-    private fun putPreference(key: String, value: T){
-        with(prefs.edit()){
-            when(value){
+    private fun putPreference(key: String, value: T) {
+        // with 函数
+        with(prefs.edit()) {
+            when (value) {
                 is Long -> putLong(key, value)
                 is Int -> putInt(key, value)
                 is Boolean -> putBoolean(key, value)
